6a596d9616ae258c2656ac88622ef70f7c59e138
David Ehlen <dehlen@me.com>
2016-04-08 13:18:40 +0200

    Initial Commit

---
+++ TRexAboutWindowControllerDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TRexAboutWindowControllerDemo
+//
+//  Created by David Ehlen on 08.04.16.
+//  Copyright © 2016 David Ehlen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TRexAboutWindowControllerDemoTests/TRexAboutWindowControllerDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  TRexAboutWindowControllerDemoTests.swift
+//  TRexAboutWindowControllerDemoTests
+//
+//  Created by David Ehlen on 08.04.16.
+//  Copyright © 2016 David Ehlen. All rights reserved.
+//
+
+import XCTest
+@testable import TRexAboutWindowControllerDemo
+
+class TRexAboutWindowControllerDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
